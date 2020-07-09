326016ef68e6d1c95eacd76af32f17f1ec04238a
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2020-07-09 17:34:51 -0400

    Add usage

--- examples/daysto.rs
+++ examples/daysto.rs
@@ -1,9 +1,31 @@
 use holiday::*;
 use chrono::*;
-use two_timer::*;
+use two_timer::parse;
+
+const USAGE: &str = r"daysto: Command line date counter. How many days until...?
+
+USAGE:
+    daysto <date>
+
+EXAMPLES:
+    daysto '2020-3-31'
+    daysto '31 March 2021'
+    daysto 'March 31, 2021'
+    daysto 'November 2'
+    daysto 'This Friday'
+    daysto Christmas
+    daysto Thanksgiving
+    daysto '1 day after tomorrow'
+    daysto '30 days ago'";
 
 fn main() {
-    let args = std::env::args().skip(1);
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+
+    if args.is_empty() {
+        eprintln!("{}", USAGE);
+        std::process::exit(1);
+    }
+
     for arg in args {
         match arg.parse::<Holiday<_>>() {
             Ok(holiday) => {
@@ -27,3 +49,4 @@ fn main() {
 fn days_to(date: NaiveDate) -> i64 {
     (date - Local::today().naive_local()).num_days()
 }
+
