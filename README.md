078c0550a23562c7886f1c451b018c8a22a112c1
Ryan O'Beirne <ryanobeirne@ryanobeirne.com>
2021-01-05 15:39:49 -0500

    Add some comments and fix some things

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # holiday
 
-A rust library for defining annually repeating dates and holidays.
+A rust library for defining and iterating over annually repeating dates and holidays.
+
+## Create a new Holiday
+
+A `Holiday` can be either a fixed date like 'April 2nd' or an nth weekday of a month, like '1st Friday in April'.
+
+```rust
+use holiday::*;
+use chrono::{Weekday, NaiveDate};
+
+// Regular `fixed` holiday
+let holiday = Holiday::new_fixed("April 2nd", April, 2);
+assert_eq!(holiday.in_year(2021), NaiveDate::from_ymd(2021, 4, 2));
+assert_eq!(holiday, NaiveDate::from_ymd(2021, 4, 2));
+assert_eq!(holiday, NaiveDate::from_ymd(2022, 4, 2));
+
+// Pastover: First Friday in April, an `nth` holiday
+let pastover = Holiday::new_nth("Pastover", First, Weekday::Fri, April);
+assert_eq!(pastover.in_year(2021), NaiveDate::from_ymd(2021, 4, 2));
+assert_eq!(pastover, NaiveDate::from_ymd(2021, 4, 2));
+assert_eq!(pastover, NaiveDate::from_ymd(2022, 4, 1));
+```
+
+## Iterate over the occurrences of a Holiday.
+
+The `HolidayIter` type is an iterator over the occurrences of a Holiday.
--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,26 @@
 //!
 //! ## Create a new Holiday
 //!
+//! A `Holiday` can be either a fixed date like 'April 2nd' or an nth weekday of a month, like '1st Friday in April'.
+//!
 //! ```rust
 //! use holiday::*;
 //! use chrono::{Weekday, NaiveDate};
-//!
-//! // Pastover: First Friday in April, an `nth` holiday
-//! let pastover = Holiday::new_nth("Pastover", First, Weekday::Fri, April);
-//! assert_eq!(pastover.in_year(2021), NaiveDate::from_ymd(2021, 4, 2));
-//! assert_eq!(pastover, NaiveDate::from_ymd(2021, 4, 2));
-//! assert_eq!(pastover, NaiveDate::from_ymd(2022, 4, 1));
 //!
 //! // Regular `fixed` holiday
 //! let holiday = Holiday::new_fixed("April 2nd", April, 2);
 //! assert_eq!(holiday.in_year(2021), NaiveDate::from_ymd(2021, 4, 2));
 //! assert_eq!(holiday, NaiveDate::from_ymd(2021, 4, 2));
 //! assert_eq!(holiday, NaiveDate::from_ymd(2022, 4, 2));
+//!
+//! // Pastover: First Friday in April, an `nth` holiday
+//! let pastover = Holiday::new_nth("Pastover", First, Weekday::Fri, April);
+//! assert_eq!(pastover.in_year(2021), NaiveDate::from_ymd(2021, 4, 2));
+//! assert_eq!(pastover, NaiveDate::from_ymd(2021, 4, 2));
+//! assert_eq!(pastover, NaiveDate::from_ymd(2022, 4, 1));
 //! ```
 
-use chrono::prelude::*;
+pub use chrono::{Local, Datelike, NaiveDate, Date, DateTime, Weekday};
 
 mod eq;
 pub mod before_after;
